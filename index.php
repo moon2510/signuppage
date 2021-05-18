6797bdaeb13fd0cbaa4e5696a0b84eeb2c84aead
moon2510 <62028865+moon2510@users.noreply.github.com>
2021-05-18 23:31:14 +0700

    v1

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Sign Up Page</title>
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+   <link rel="stylesheet" href="/CSS/index.css">
+</head>
+<body>
+   <div class="column">
+      <div class="left">
+         <div class="logo">
+            <h1>Whitespace</h1>
+         </div>
+         <div class="text">
+            <h2>Discover the world's top Designers and Creatives.</h2>
+         </div>
+         <div class="image">
+            <img src="/Image/1.png" alt="">
+         </div>
+      </div>
+      <div class="right">
+         <div></div>
+         <div class="center">
+            <div class="sign">
+               <h3>Sign up to Whitespace</h3>
+            </div>
+            <div class="social">
+               <div class="google">
+                  <button class="gmail"><i class="fab fa-google"></i>Sign up with Google</button>
+               </div>
+               <div class="icon">
+                  <button class="twitter"><i class="fab fa-twitter"></i></button>
+                  <button class="facebook"><i class="fab fa-facebook-square"></i></button>
+               </div>
+               
+            </div>
+            <div class="orgray">
+               <div class="ortwo">
+                  <hr width="100%" size="1px" align="left" color="gainsboro" />
+                  
+               </div>
+               <div class="or">Or</div>
+               <div class="ortwo">
+                  <hr width="100%" size="1px" align="left" color="gainsboro" />
+   
+               </div>
+            </div>
+            <div class="name">
+               <div class="fullname">
+                  <h5>Full Name</h5>
+                  <form action="" ><input type="text" placeholder="Bui Thi Minh Nguyet"></form>
+               </div>
+               <div class="username">
+                  <h5>Username</h5>
+                  <form action=""><input type="text" placeholder="@doathon"></form>
+               </div>
+            </div>
+            <div class="address">
+               <h5>Email Address</h5>
+               <form action=""><input type="text" placeholder="maysechetneudungvaotao@gmail.com"></form>
+            </div>
+            <div class="password">
+               <h5>Password</h5>
+               <form action=""><input type="text" placeholder="6+ Characters"></form>
+            </div>
+            <div class="check">
+               <form action=""><input type="checkbox" value="true" >Create an account means you're okay with our <strong>Terms of Service, 
+                  Privacy Policy</strong>, and our default <strong>Notification Settings</strong></form>
+            </div>
+            <div class="create">
+               <button>Create Account</button>
+            </div>
+            <div class="note">
+               <h6>This site is protected by reCAPCHA and the Google <strong>Privacy
+                     Policy</strong>, and <strong>Terms of Service</strong> apply</h6>
+            </div>
+         </div>
+         
+         <div></div>
+      </div>
+   </div>
+</body>
+</html>
